c165879581e80283a3b16e9c6243a277f1901ded
krishna suthan <krishnask6@icloud.com>
2016-03-30 23:51:31 +0100

    screen layouts

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('book', {
+        url: '/book',
+        templateUrl: 'app/book/BookConsultation.html',
+        controller: 'BookConsultationController',
+        controllerAs: 'book'
       });
 
     $urlRouterProvider.otherwise('/');
