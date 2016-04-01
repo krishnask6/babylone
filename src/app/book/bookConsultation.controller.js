5b9f3c6387b847f91dc23f4285c6dca7de26f002
krishna suthan <krishnask6@icloud.com>
2016-04-01 11:46:26 +0100

    checkbox states selection

--- src/app/book/bookConsultation.controller.js
+++ src/app/book/bookConsultation.controller.js
@@ -1,61 +1,76 @@
-(function() {
+(function () {
     'use strict';
 
-    angular
+        angular
         .module('babylone')
         .controller('BookConsultationController', BookConsultationController);
 
-    /** @ngInject */
-    function BookConsultationController($timeout, $log, consultationService) {
-        var vm = this;
+        /** @ngInject */
+        function BookConsultationController($timeout, $log, consultationService) {
+            var vm = this;
 
-        /** Service to get the patients/user & releative list & details*/
-        consultationService.getPatients().then(function(data){
-            vm.patients = data;
-        })
-
-        /** Service to get the professional health care list & details*/
-        consultationService.gethealthCareProfession().then(function(data){
-            vm.healthCareProfessional = data;
-        })
-
-        /** Service to get the professional health care list & details*/
-        consultationService.getDoctorDetails().then(function(data){
-            vm.doctors = data;
-        })
-
-        vm.items = ['item1', 'item2', 'item3'];
-        vm.open = open;
-        vm.updateSelection = updateSelection;
-
-        /** Using ui-bootstrap modal*/
-        function open(size) {
-            var modalInstance = $uibModal.open({
-                animation: true,
-                templateUrl: 'modalTemplate.html',
-                controller: 'ModalController',
-                controllerAs: 'vm',
-                windowClass : 'show',
-                size: size,
-                resolve: {
-                    items: function () {
-                        return vm.items;
-                    }
-                }
+            /** Service to get the patients/user & releative list & details*/
+            consultationService.getPatients().then(function (data){
+                vm.patients = data;
             });
 
-            modalInstance.result.then(function (selectedItem) {
+            /** Service to get the professional health care list & details*/
+            consultationService.gethealthCareProfession().then(function (data){
+                vm.healthCareProfessionals = data;
+            });
+
+            /** Service to get the professional health care list & details*/
+            consultationService.getDoctorDetails().then(function (data){
+                vm.doctors = data;
+            });
+
+            vm.items = ['item1', 'item2', 'item3'];
+            vm.open = open;
+            vm.updateSelection = updateSelection;
+
+            init();
+
+            function init(){
+                // initialise something here
+            }
+
+            /** Using ui-bootstrap modal
+             *  {string} size size of the modal window
+            */
+            function open(size) {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'modalTemplate.html',
+                    controller: 'ModalController',
+                    controllerAs: 'vm',
+                    windowClass : 'show',
+                    size: size,
+                    resolve: {
+                        items: function () {
+                            return vm.items;
+                        }
+                    }
+                });
+
+                modalInstance.result.then(function (selectedItem) {
                     vm.selected = selectedItem;
                 }, function () {
                     $log.info('Modal dismissed at: ' + new Date());
                 });
-        }
+            }
 
-        function updateSelection(position, entities) {
-          angular.forEach(entities, function(patients, index) {
-            if (position != index)
-              patients.selected = false;
-          });
+            /**
+             *  updateSelection - updates the check status of the item selected
+             *  and thus unselects rest of the item
+             *  @param {int} position position of the item selected
+             *  @param {Array} items list of items in the checkbox list
+            */
+            function updateSelection(position, items) {
+                angular.forEach(items, function (item, index) {
+                    if (position !== index) {
+                        item.selected = false;
+                    }
+                });
+            }
         }
-    }
 })();
